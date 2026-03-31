f842cfd5abcee580cb9222b570a6285334b0e7b5
jjjk2011 <67122504+jjjk2011@users.noreply.github.com>
2026-03-31 00:13:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,26 @@
-const CACHE_NAME = 'audiobase-v2';
-const assets = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'audiobase-pro-v13';
+const assets = [
+  './',
+  './index.html',
+  './fase.html',
+  './delay.html',
+  './projeto.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
